e78c1c55a8ad2d87109ed8fef3c76274e0981eb5
Grenish rai <ariestheracer@gmail.com>
2023-09-01 01:29:35 +0530

    Update BackgroundImage.jsx

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { cccoil } from '../assets'
+import React from "react";
+
+import { cccoil8 } from "../assets";
 
 const BackgroundImage = () => {
   return (
-    <div className='w-full h-screen absolute overflow-hidden z-[-9999]'>
-        <img src={cccoil} className="roundImage w-[1500px] absolute top-[-550px] left-[800px]" />
+    <div className="w-full h-[100%] md:h-[100%] absolute overflow-hidden z-[1]">
+      <img
+        src={cccoil8}
+        className="roundImage w-[1500px] absolute top-[350px] left-[-130px] md:top-[-800px] md:left-[-600px] dark:opacity-10 opacity-20 transition-all ease-in-out duration-500"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default BackgroundImage
+export default BackgroundImage;
