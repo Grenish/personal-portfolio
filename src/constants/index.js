11e4189c790fc86be8cabbd404184753c6a81e18
Grenish rai <ariestheracer@gmail.com>
2023-09-01 01:32:33 +0530

    Import/Export social media links

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+import { github, linkedin, twitterx, instagram } from "../assets/socials";
+
+export const socialMediaLinks = [
+  {
+    name: "Github",
+    link: "https://github.com/Grenish",
+    icon: github,
+  },
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/grenish-rai-71a5b4219/",
+    icon: linkedin,
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/grenish_rai",
+    icon: twitterx,
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/grenish_rai/",
+    icon: instagram,
+  },
+];
