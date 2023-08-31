436ef21ff771a7d3ffa4f522a9b81710e15cb9b1
Grenish rai <ariestheracer@gmail.com>
2023-09-01 01:29:00 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,14 +2,25 @@ import cccoil from "./cccoil.svg";
 import grenish from "./grenish.jpg";
 import grenishLogoBlack from "./grenishLogoBlack.svg";
 import grenishLogoWhite from "./grenishLogoWhite.svg";
-import grenishFavidon from "./grenishFavicon.svg";
-import grenishFavicon2 from "./grenishFavicon2.png";
+import cccoil2 from "./cccoil2.svg";
+import cccoil3 from "./cccoil3.svg";
+import cccoil4 from "./cccoil4.svg";
+import cccoil5 from "./cccoil5.svg";
+import cccoil6 from "./cccoil6.svg";
+import cccoil7 from "./cccoil7.svg";
+import cccoil8 from "./cccoil8.svg";
+
 
 export {
   cccoil,
   grenish,
   grenishLogoBlack,
   grenishLogoWhite,
-  grenishFavidon,
-  grenishFavicon2,
+  cccoil2,
+  cccoil3,
+  cccoil4,
+  cccoil5,
+  cccoil6,
+  cccoil7,
+  cccoil8,
 };
