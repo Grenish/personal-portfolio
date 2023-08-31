cdbebab6baf9d8d18ad956605ba316c49b436e16
Grenish rai <ariestheracer@gmail.com>
2023-08-31 10:45:22 +0530

    import/export assets

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import cccoil from "./cccoil.svg";
+import grenish from "./grenish.jpg";
+import grenishLogoBlack from "./grenishLogoBlack.svg";
+import grenishLogoWhite from "./grenishLogoWhite.svg";
+import grenishFavidon from "./grenishFavicon.svg";
+import grenishFavicon2 from "./grenishFavicon2.png";
+
+export {
+  cccoil,
+  grenish,
+  grenishLogoBlack,
+  grenishLogoWhite,
+  grenishFavidon,
+  grenishFavicon2,
+};
