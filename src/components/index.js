73a8ab556c3eb1dd3332d5e7e2035373bba259d5
Grenish rai <ariestheracer@gmail.com>
2023-08-31 10:46:20 +0530

    Import/export components

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,15 @@ import Footer from "./Footer";
 import Contact from "./Contact";
 import Home from "./Home";
 import BlogPosts from "./BlogPosts";
+import BackgroundImage from "./BackgroundImage";
 
-export { Navbar, Projects, About, Footer, Contact, Home, BlogPosts };
+export {
+  Navbar,
+  Projects,
+  About,
+  Footer,
+  Contact,
+  Home,
+  BlogPosts,
+  BackgroundImage,
+};
