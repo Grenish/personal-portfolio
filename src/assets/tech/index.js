bf6c3830b93c0d24936fe664e67de675509684cc
Grenish rai <ariestheracer@gmail.com>
2023-09-01 01:29:30 +0530

    Added tech

---
+++ src/assets/tech/index.js
@@ -0,0 +1,9 @@
+import css from "./css.png";
+import html from "./html.png";
+import javascript from "./javascript.png";
+import react from "./react.png";
+import tailwindcss from "./tailwindcss.png";
+import nodejs from "./nodejs.png";
+import figma from "./figma.png";
+
+export { css, html, javascript, react, tailwindcss, nodejs, figma };
