1fceb4cd2f515e42c86e6d8231b0a3466e7fc33f
Grenish rai <ariestheracer@gmail.com>
2023-08-31 10:46:29 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { grenishLogoBlack, grenishLogoWhite } from "../assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <>
+      <header className="w-full p-4 fixed ">
+        <nav className="w-[60%] m-auto bg-[#e8eddf26] backdrop-blur-xl p-3 rounded-full flex items-center justify-between">
+          <div className="flex items-center justify-center ml-2">
+            <Link to="/">
+              <img src={grenishLogoWhite} className="w-[150px]" />
+            </Link>
+          </div>
+          <div className="mr-2">
+            <ul className="flex space-x-3">
+              <Link to="/about">
+                <li className="p-2 bg-[#0d182192] hover:bg-[#0d1821] transition rounded-full text-platinum">
+                  About
+                </li>
+              </Link>
+              <Link to="/projects">
+                <li className="p-2 bg-[#0d182192] hover:bg-[#0d1821] transition rounded-full text-platinum">
+                  Projects
+                </li>
+              </Link>
+              <Link to="/contact">
+                <li className="p-2 bg-[#0d182192] hover:bg-[#0d1821] transition rounded-full text-platinum">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
