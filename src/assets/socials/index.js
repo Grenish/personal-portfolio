face68d2f13c3aeaf2486c58449816637ec7292c
Grenish rai <ariestheracer@gmail.com>
2023-09-01 01:29:14 +0530

    Added socials

---
+++ src/assets/socials/index.js
@@ -0,0 +1,6 @@
+import github from "./github.png";
+import linkedin from "./linkedin.png";
+import twitterx from "./twitterx.png";
+import instagram from "./instagram.png";
+
+export { github, linkedin, twitterx, instagram };
