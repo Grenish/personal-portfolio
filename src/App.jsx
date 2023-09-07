841737269dec80ca0b640919aad017f84be28c80
Grenish rai <ariestheracer@gmail.com>
2023-09-08 01:29:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,26 @@ import {
   Contact,
   Footer,
   Projects,
+  Success,
+  Error,
 } from "./components";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 
 const App = () => {
+  const { pathname } = useLocation();
   return (
     <div className="bg-dark dark:bg-alabaster transition-colors">
-      
-      <Navbar />
+      {pathname !== "/error" && pathname !== "/success" && <Navbar />}
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<BlogPosts />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
+        {pathname === "/error" && <Route path="/error" element={<Error />} />}
+        {pathname === "/success" && (
+          <Route path="/success" element={<Success />} />
+        )}
       </Routes>
       <Footer />
     </div>
