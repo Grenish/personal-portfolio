71b46afaa5ec423a6b1a3f2cffe61a5635335548
Grenish rai <ariestheracer@gmail.com>
2023-09-07 12:21:52 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,13 @@
 import { github, linkedin, twitterx, instagram } from "../assets/socials";
+import {
+  html,
+  css,
+  figma,
+  javascript,
+  react,
+  tailwindcss,
+  nodejs,
+} from "../assets/tech";
 
 export const socialMediaLinks = [
   {
@@ -22,3 +31,49 @@ export const socialMediaLinks = [
     icon: instagram,
   },
 ];
+
+export const techStack = [
+  {
+    name: "HTML",
+    icon: html,
+    desc: "I have been working with HTML for 2 years now. I have used HTML in many of my projects and I am very comfortable with it.",
+    url: "https://www.w3schools.com/html/",
+  },
+  {
+    name: "CSS",
+    icon: css,
+    desc: "I have been working with CSS for 2 years now. I have used CSS in many of my projects and I am very comfortable with it.",
+    url: "https://www.w3schools.com/css/",
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+    desc: "I have been working with JavaScript for 2 years now. I have used JavaScript in many of my projects and I am very comfortable with it.",
+    url: "https://www.w3schools.com/js/DEFAULT.asp",
+  },
+  {
+    name: "React",
+    icon: react,
+    desc: "I have been working with React for 1 year now. I have used React in many of my projects and I am very comfortable with it.",
+    url: "https://reactjs.org/",
+  },
+  {
+    name: "Tailwindcss",
+    icon: tailwindcss,
+    desc: "I have been working with Tailwindcss for 1 year now. I have used Tailwindcss in many of my projects and I am very comfortable with it.",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    name: "Nodejs",
+    icon: nodejs,
+    desc: "I have been working with Nodejs for 1 year now. I have used Nodejs in many of my projects and I am very comfortable with it.",
+    url: "https://nodejs.org/en/",
+  },
+  {
+    name: "Figma",
+    icon: figma,
+    desc: "I have been working with Figma for 1 year now. I have used Figma in many of my projects and I am very comfortable with it.",
+    url: "https://www.figma.com/",
+  },
+  
+]
