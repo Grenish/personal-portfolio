047c31b8ece177ea62a3cde73aede42b82d344fc
Grenish rai <ariestheracer@gmail.com>
2023-09-07 22:16:46 +0530

    Merged cards separately

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
 import { grenish2 } from "../assets";
-import { Card } from "../Cards";
 import { techStack } from "../constants";
 import { Transition } from "@headlessui/react";
+
+const Card = ({ id, name, icon, desc }) => {
+  return (
+    <div>
+      <div
+        className="flex relative items-center border-4 sm:h-20 h-24 cursor-pointer overflow-hidden border-vanilla dark:border-saffron rounded-2xl p-3 group"
+        key={id}
+      >
+        <img
+          src={icon}
+          alt={name}
+          className=" transition-all duration-500 ease-in-out transform group-hover:-translate-x-2 group-hover:-translate-y-2"
+        />
+        <img
+          src={icon}
+          alt={name}
+          className="absolute -right-5 top-2 opacity-20 w-20 -rotate-45 transition-all duration-500 group-hover:scale-110 ease-in-out transform group-hover:opacity-100 "
+        />
+        <div className="ml-5 transition-all duration-500 delay-100 ease-in-out transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+          <h2 className="text-platinum dark:text-night">{name}</h2>
+          <p className="text-xs text-gray-500 dark:text-night">{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const About = () => {
   return (
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
-import { ProjectCards } from "../Cards";
+
+const ProjectCards = ({ id, name, image, desc, url }) => {
+  return (
+    <div>
+      <a href={url} key={id}>
+        <div className="relative group border-4 cursor-pointer overflow-hidden border-vanilla dark:border-saffron rounded-2xl p-3 h-full dark:text-gray-100 md:h-3/4">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-lg transform transition-all duration-200 ease-linear group-hover:opacity-25 group-hover:scale-110"
+          />
+          <div className="absolute inset-0 bg-transparent bg-opacity-25 flex flex-col items-center justify-center transition-all duration-200 group-hover:bg-transparent">
+            <h2 className="sm:text-xl text-lg border-4 bg-[#3335334a] backdrop-blur-sm group-hover:backdrop-blur-none group-hover:bg-transparent border-jasmine dark:bg-[#EFDC9E4a] group-hover:border-transparent dark:text-night group-hover:text-platinum group-hover:dark:text-jet p-2 rounded-xl md:text-3xl text-alabaster font-bold">
+              {name}
+            </h2>
+            <p className="text-xs opacity-0 group-hover:opacity-100 md:text-sm text-platinum dark:text-jet">
+              {desc}
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
 
 const Projects = () => {
   return (
