8c7f2cfa87482e48f7e2bc8509345487531a67a2
Grenish rai <ariestheracer@gmail.com>
2023-09-07 20:40:15 +0530

    Create index.js

---
+++ src/assets/projects/index.js
@@ -0,0 +1,19 @@
+import bcaResults from './bcaResults.png';
+import coderjojo from './coderjojoClient.png';
+import codersclub from './codersclub.png';
+import exsolve from './exsolve.png';
+import hoobank from './hoobank.png';
+import imageflex from './imageflex.png';
+import studystuffs from './studystuffs.png';
+import zane from './zane.png';
+
+export {
+    bcaResults,
+    coderjojo,
+    codersclub,
+    exsolve,
+    hoobank,
+    imageflex,
+    studystuffs,
+    zane,
+}
