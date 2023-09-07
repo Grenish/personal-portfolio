a95d292a95909a54398812fee46ed1f70d2f9af8
Grenish rai <ariestheracer@gmail.com>
2023-09-07 22:25:06 +0530

    added netlify forms

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,24 +2,102 @@ import React from "react";
 import { SectionWrapper } from "../hoc";
 
 const Contact = () => {
+  // Method to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement function to process the form submission
+  };
+
+  // Method to manage input text state
+  const handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <div>
-      <SectionWrapper>
-        <div className="mb-10">
-          <h2 className="text-3xl font font-extraBold text-platinum dark:text-night mb-4">
-            Contact Me
-          </h2>
-          <p className="text-gray-400 dark:text-gray-800 text-justify mt-4">
-            You can reach me through the following:
-          </p>
-        </div>
-        <div className=" flex flex-col justify-center items-center space-y-5">
-          <div className="flex flex-row justify-center items-center space-x-5">
-            
+    <SectionWrapper>
+      <div className="mb-10">
+        <h2
+          id="contact_header"
+          className="text-3xl font font-extraBold text-platinum dark:text-night mb-4"
+        >
+          Contact Me
+        </h2>
+        <p
+          className="text-gray-400 dark:text-gray-800 text-justify mt-4"
+          aria-labelledby="contact_header"
+        >
+          You can reach me through the following form:
+        </p>
+      </div>
+      <div className="flex flex-col justify-center items-center space-y-5">
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit={handleSubmit}
+          className="w-full max-w-lg"
+        >
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="name"
+                type="text"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                id="email"
+                type="email"
+                onChange={handleChange}
+              />
+            </div>
           </div>
-        </div>
-      </SectionWrapper>
-    </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="message"
+              >
+                Message
+              </label>
+              <textarea
+                className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white h-48 resize-none"
+                id="message"
+                onChange={handleChange}
+              ></textarea>
+            </div>
+          </div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3">
+              <button
+                className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Send
+              </button>
+            </div>
+            <div className="md:w-2/3"></div>
+          </div>
+        </form>
+      </div>
+    </SectionWrapper>
   );
 };
 
