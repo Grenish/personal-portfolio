6a5b149b35b6bef64de05a1a2b8b1b5f2678f9b1
Grenish rai <ariestheracer@gmail.com>
2023-09-08 01:29:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { grenishLogoWhite, grenishLogoBlack } from "../assets";
 
 const Footer = () => {
   return (
     <div>
-      Footer
+      <footer className="flex justify-center">
+        <div className="flex flex-col items-center justify-center space-y-2 w-11/12 md:w-3/4 xl:w-7/12 border-t border-platinum dark:border-jet p-5">
+          <p className="text-center text-platinum dark:text-dark">
+            Made with passion &copy; All Rights Reserved
+          </p>
+          <p className="text-center text-platinum dark:text-dark text-xs">2023</p>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
