5c50ef50d2f58fc84bf1ae80fc405d05f2fbd72e
Grenish rai <ariestheracer@gmail.com>
2023-12-29 22:28:24 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,24 @@
-import React, { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import { motion } from "framer-motion";
+import React from "react";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
-    <div>
-      <header className="px-28 relative">
-        <nav className="p-5 flex justify-between items-center">
-          <div className="font-sat text-2xl">Grenish Rai</div>
-
-          <div
-            className={`bg-white-200 text-dark rounded-full w-[40px] h-[40px] flex justify-center items-center relative`}
-            aria-label={isOpen ? "Close menu" : "Open menu"}
-            onClick={toggleMenu}
-          >
-            {isOpen ? (
-              <CloseIcon className="text-2xl cursor-pointer" />
-            ) : (
-              <MenuIcon className="text-2xl cursor-pointer" />
-            )}
-
-            {isOpen && (
-              <div className={`${isOpen ? "w-[140px] h-[140px]" : "w-[40px] h-[40px]"} absolute bg-white-200 top-0 right-0 z-[-1] rounded-full transition ease-in-out duration-200`}>
-                Hello World
-              </div>
-            )}
-          </div>
-        </nav>
-      </header>
-    </div>
+    <header className="w-full p-4 fixed top-0 left-0">
+      <nav className="w-[90%] m-auto flex items-center justify-between">
+        <span className="text-[32px] w-auto ahs cursor-pointer text-white-200 hover:text-gray-400">
+          Gr
+        </span>
+        <span>
+          <ul className="flex items-center">
+            <li className="text-white-200 hover:text-gray-400 px-5 cursor-pointer cor text-sm font-semibold uppercase">
+              About
+            </li>
+            <li className="text-white-200 hover:text-gray-400 px-5 cursor-pointer cor text-sm font-semibold uppercase">
+              Contact
+            </li>
+          </ul>
+        </span>
+      </nav>
+    </header>
   );
 };
 
