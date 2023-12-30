5f2c4d1e9af55284c0421fa28190d3da0db17dc3
Grenish rai <ariestheracer@gmail.com>
2023-12-30 18:49:47 +0530

    Added hambuger [Bug]

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const Hamburger = () => {
+
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <button onClick={() => setOpen((prev) => !prev)} className="">
+      <input type="checkbox" id="checkbox" />
+      <label htmlFor="checkbox" className="toggle">
+        <div className="bars" id="bar1"></div>
+        <div className="bars" id="bar2"></div>
+        <div className="bars" id="bar3"></div>
+      </label>
+    </button>
+  );
+};
 
 const Navbar = () => {
+
+  const [isOpen, setOpen] = useState(false);
+
+
   return (
-    <header className="w-full p-4 fixed top-0 left-0">
-      <nav className="w-[90%] m-auto flex items-center justify-between">
+    <header className="w-full p-4 fixed z-50 top-0 left-0">
+      <nav className="w-[90%] m-auto flex items-center justify-between relative">
         <span className="text-[32px] w-auto ahs cursor-pointer text-white-200 hover:text-gray-400">
           Gr
         </span>
         <span>
-          <ul className="flex items-center">
+          <ul className="sm:flex hidden items-center">
             <li className="text-white-200 hover:text-gray-400 px-5 cursor-pointer cor text-sm font-semibold uppercase">
               About
             </li>
@@ -17,6 +37,13 @@ const Navbar = () => {
             </li>
           </ul>
         </span>
+
+        <div className="block sm:hidden">
+          <Hamburger />
+          {isOpen && (
+            <div className="absolute top-0 bg-rose-500">Hello World</div>
+          )}
+        </div>
       </nav>
     </header>
   );
