0b204ca9c0233f2ca32249c26ce0527521bf4548
Grenish rai <ariestheracer@gmail.com>
2024-03-08 12:05:28 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <div className="p-2 bg-night flex justify-between items-center cor text-sm sm:text-md">
+        <span>&copy; {new Date().getFullYear()} Grenish Rai - Made with passion</span>
+        <span>mrcoder2033d@gmail.com</span>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
