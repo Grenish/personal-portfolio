3514c4783be5199cbd27a358fa284c6329eb18d5
Grenish rai <ariestheracer@gmail.com>
2024-03-10 16:17:49 +0530

    Add new project images and update index.js

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,4 +1,8 @@
 import zane from "./zane.png";
 import luna from "./luna.png";
+import dashboard from "./dashboard.jpeg";
+import webchitchat from "./webchitchat.jpeg";
+import imageflex from "./imageflex.png";
+import codersclub from "./codersclub.png";
 
-export { zane, luna };
+export { zane, luna, dashboard, webchitchat, imageflex, codersclub };
