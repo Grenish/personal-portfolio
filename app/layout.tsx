3222a618faec6eb7952c7435406f0052eab73641
Grenish rai <ariestheracer@gmail.com>
2024-06-10 20:31:29 +0530

    Added Nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter, Poppins, Montserrat } from "next/font/google";
+import "./globals.css";
+import { Providers } from "./providers";
+
+// const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Grenish Rai",
+  description:
+    "meet Grenish Rai's portfolio. A front-end developer and designer. Currently pursuing BCA in Computer Applications from SMIT aka. Sikkim Manipal Institute of Technology.",
+  authors: [{ name: "Grenish Rai" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl">Hi, Im Grenish.</h1>
+    </div>
+  )
+}
