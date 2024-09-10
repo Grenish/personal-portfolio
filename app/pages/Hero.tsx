024475578b6e6c648221e756c12472d0746e72fa
Grenish rai <ariestheracer@gmail.com>
2024-09-10 19:27:48 +0530

    chore: Add Hero component with introductory text

---
+++ app/pages/Hero.tsx
@@ -0,0 +1,14 @@
+
+export default function Hero() {
+  return (
+    <div className="hero-body relative">
+      <h1 className="hero-title text-6xl font-bold">Hi, I'm Grenish Rai</h1>
+      <h2 className="text-xl text-gray-300 font-semibold">
+        A Full Stack Web Developer & Designer
+      </h2>
+      <p className="text-lg text-gray-500 mt-5">
+        Innovating through code, shaping tomorrow's tech
+      </p>
+    </div>
+  );
+}
