3958c9e6c55291dfad59f94bf960e2c2b268eec7
Grenish rai <ariestheracer@gmail.com>
2024-09-15 17:17:17 +0530

    feat: Add About page with floating dock and content

---
+++ app/pages/About.tsx
@@ -0,0 +1,82 @@
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+
+import FloatingDock from "../components/floating-dock";
+
+export default function About() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Aceternity UI",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Changelog",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className="h-screen bg-black relative">
+      <h1 className="text-white">Little About Me</h1>
+      <p className="text-white">
+        Currently I'm a student at Sikkim Manipal Institute of Technology,
+        pursuing my BCA degree. I started web tech as fun and now it's my
+        passion.
+      </p>
+      <p className="text-white">
+        I'm bit of a poetical person, I love to write poetries and stories.
+      </p>
+
+      <FloatingDock />
+    </div>
+  );
+}
