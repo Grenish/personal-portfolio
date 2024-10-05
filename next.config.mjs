0d1f995285e2ec617713eea13a031142964ffc73
Grenish rai <ariestheracer@gmail.com>
2024-10-05 18:03:00 +0530

    feat: Update next.config.mjs to include "api.daily.dev" as an allowed image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "i.pinimg.com"],
+    domains: ["images.unsplash.com", "api.daily.dev" ],
   },
 };
 
