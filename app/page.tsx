a3c3998f199740579be33f95ac78b579ddb6673a
Grenish rai <ariestheracer@gmail.com>
2024-10-05 18:03:21 +0530

    feat: Add Footer and Advertisement components
    - Added Footer component to display the website footer.
    - Added Advertisement component to display ads on the page.
    - Updated Page component to include the Footer and Advertisement components.

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,23 @@ import {
   IconBrandLinkedin,
   IconWorldWww,
   IconDownload,
-  IconBlockquote
-
+  IconBlockquote,
+  IconMailSpark,
 } from "@tabler/icons-react";
 import LoadingScreen from "./components/LoadingScreen";
 import About from "./pages/About";
 import Hero from "./pages/Hero";
 import TechStack from "./pages/TechStack";
 import { FloatingDock } from "@/components/ui/floating-dock";
+import Footer from "./pages/Footer";
+import Advertisement from "./pages/Ads";
 
 export default function Page() {
-  const homeRef = useRef(null);
+  const homeRef = useRef<HTMLDivElement>(null);
+  const footerRef = useRef<HTMLDivElement>(null);
+
   const isHomeInView = useInView(homeRef, { amount: 0.5 });
+  const isFooterInView = useInView(footerRef, { amount: 0.1 });
 
   const variants = {
     hidden: { opacity: 0, scale: 0.5, y: 100, x: "-50%" },
@@ -46,32 +51,34 @@ export default function Page() {
     {
       title: "Projects",
       icon: <IconWorldWww className="h-full w-full text-neutral-300" />,
-      href: "#",
+      href: "/projects",
     },
     {
       title: "Blogs",
       icon: <IconBlockquote className="h-full w-full text-neutral-300" />,
-      href: "#",
+      href: "https://dev.to/grenishrai",
+    },
+    {
+      title: "Contact",
+      icon: <IconMailSpark className="h-full w-full text-neutral-300" />,
+      href: "mailto:mrcoder2033d@gmail.com",
     },
     {
       title: "LinkedIn",
       icon: <IconBrandLinkedin className="h-full w-full text-neutral-300" />,
-      href: "#",
+      href: "https://www.linkedin.com/in/grenish-rai/",
     },
-    {
-      title: "Twitter",
-      icon: <IconBrandX className="h-full w-full text-neutral-300" />,
-      href: "#",
-    },
+
     {
       title: "GitHub",
       icon: <IconBrandGithub className="h-full w-full text-neutral-300" />,
-      href: "#",
+      href: "https://github.com/Grenish",
     },
     {
       title: "Download",
       icon: <IconDownload className="h-full w-full text-neutral-300" />,
-      href: "#",
+      href: "/grenishrai.pdf",
+      Download: true,
     },
   ];
 
@@ -80,10 +87,12 @@ export default function Page() {
       <Hero ref={homeRef} />
       <About />
       <TechStack />
+      <Advertisement />
+      <Footer ref={footerRef} />
       <motion.div
         variants={variants}
         initial="hidden"
-        animate={!isHomeInView ? "visible" : "hidden"}
+        animate={!isHomeInView && !isFooterInView ? "visible" : "hidden"}
         className="fixed bottom-10 sm:left-1/2 left-auto transform -translate-x-1/2 sm:right-auto right-5 z-50"
       >
         <FloatingDock items={links} desktopClassName="bg-neutral-900" />
