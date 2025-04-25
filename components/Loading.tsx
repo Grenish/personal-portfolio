54626c4e26318d0c836fce930cf74c1b10aa88ed
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-04-25 23:03:05 +0530

    added component

---
+++ components/Loading.tsx
@@ -0,0 +1,8 @@
+export default function LoadingScreen() {
+  return (
+    <div className="w-full h-screen bg-black flex items-center justify-center flex-col">
+      <h2 className="text-9xl wave pointer-events-none">👋</h2>
+      <p className="text-white mt-10 capitalize">Preparing the best experience for you</p>
+    </div>
+  );
+}
