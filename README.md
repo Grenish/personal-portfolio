6b2bb12ede0674007ba3ce4310ecf6b0d35f275d
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-04-25 22:55:42 +0530

    scratch

--- README.md
+++
@@ -1,5 +0,0 @@
-## Grenish Rai
-
-![Grenish rai portfolio](https://github.com/user-attachments/assets/75b229a5-72df-45cb-b57b-02bfea276872)
-
-This is my personal porfolio created in nextjs and tailwindcss.
--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function loading() {
-  return (
-    <div className="w-full h-screen bg-black flex items-center justify-center flex-col">
-      <h2 className="text-9xl wave pointer-events-none">👋</h2>
-      <p className="text-white mt-10 capitalize">Preparing the best experience for you</p>
-    </div>
-  );
-}
