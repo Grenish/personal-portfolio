94a733a0c9127c0d6a453547eae651d7e34cdad0
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-08-04 22:24:27 +0530

    feat: add image remote patterns for external sources in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+      },
+      {
+        protocol: "https",
+        hostname: "dev-to-uploads.s3.amazonaws.com",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
