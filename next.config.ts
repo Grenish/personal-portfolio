dbcf3544bab64a9b49ead368a8d45ed70563fd71
Grenish rai <107925840+Grenish@users.noreply.github.com>
2025-08-05 10:38:38 +0530

    feat: add api.daily.dev to remotePatterns for image optimization

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "dev-to-uploads.s3.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "api.daily.dev",
+      },
     ],
   }
 };
